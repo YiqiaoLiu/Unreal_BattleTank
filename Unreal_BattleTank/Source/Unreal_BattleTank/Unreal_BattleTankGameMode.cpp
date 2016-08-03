fbeffce6f3df02116ba0aa7d2ebbdf498a060531
YiqiaoLiu <yiqiao.liu@duke.edu>
2016-08-03 16:10:04 -0700

    Add a landscape

---
+++ Unreal_BattleTank/Source/Unreal_BattleTank/Unreal_BattleTankGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Unreal_BattleTank.h"
+#include "Unreal_BattleTankGameMode.h"
+
+
+
+
---
+++ Unreal_BattleTank/Source/Unreal_BattleTank/Unreal_BattleTankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Unreal_BattleTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_BATTLETANK_API AUnreal_BattleTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
